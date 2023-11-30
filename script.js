1d30cc6277ac7443bd50bd0894c378aed660eacc
Vitaliy Mlynetskyi <mlineckiyvitaliy@gmail.com>
2023-11-30 23:42:42 +0200

    Second commit

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-const a = document.querySelector(".addTodo");
+const addtodo = document.querySelector(".addTodo");
 const button = document.querySelector(".addButton");
 const todobox = document.querySelector(".todoBox");
 button.onclick = addTodos;
-//
+let count = 1;
+
 function addTodos(evt) {
-    let data = a.value;
-    todobox.innerHTML += `<input type="checkbox" id="death" name="death"><label for="death">${data}</label>`
-    a.value = '';
+    let data = addtodo.value;
+    todobox.innerHTML += `<input type="checkbox" id="${"checktodo" + count}"> <label for="${"checktodo" + count}">${data}</label>`
+    count++;
+    addtodo.value = '';
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    todobox.addEventListener('change', (event) => {
+        todoChecked(event.target)
+    });
+});
+
+
+function todoChecked(checkbox){
+    checkbox.checked ? console.log(checkbox.id + " yes") : console.log(checkbox.id + " no");
 }
